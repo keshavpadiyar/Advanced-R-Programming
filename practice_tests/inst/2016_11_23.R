08f06fff7acc09b959ada922e856f1345129ee6d
Keshav Padiyar <keshavpadiyar.07@gmail,com>
2020-10-19 00:47:48 +0200

    updated practice test 2016_11_23.R

--- practice_tests/inst/2016_11_23.R
+++ practice_tests/inst/2016_11_23.R
@@ -1,7 +1,7 @@
 gen_shape <- function(){
     t = seq(1,2*pi,0.1)
     x = 16 * sin(t^3)
-    y = 13 * cos(t) − 5 * cos(2 * t) − 2 * cos(3 * t) − cos(4 * t)
+    y = 13 * cos(t)-5 * cos(2 * t) -2 * cos(3 * t) - cos(4 * t)
     return(data.frame(t=t, x =x, y=y))
 }
 
